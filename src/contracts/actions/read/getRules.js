0cc7a920528422b32aae0cf03d329a4372b44430
Tomoya Nagasawa <40726926+ocrybit@users.noreply.github.com>
2022-07-11 18:39:12 +0400

    add access control (#8)
    * add access control
    
    * remove unnecessary packages

--- src/contracts/actions/read/getRules.js
+++ src/contracts/actions/read/getRules.js
@@ -1,11 +1,11 @@
 import { isNil, mergeLeft } from "ramda"
 import { err, parse, mergeData } from "../../lib/utils"
 
-export const getSchema = async (state, action) => {
+export const getRules = async (state, action) => {
   let { _data, data, query, new_data, path } = await parse(
     state,
     action,
-    "getSchema"
+    "getRules"
   )
-  return { result: _data.schema || null }
+  return { result: _data.rules || null }
 }
--- src/contracts/lib/utils.js
+++ src/contracts/lib/utils.js
@@ -1,16 +1,111 @@
-import { concat, without, isNil, slice, includes, is, complement } from "ramda"
+import {
+  intersection,
+  concat,
+  without,
+  isNil,
+  slice,
+  includes,
+  is,
+  complement,
+  clone,
+} from "ramda"
 import { validator } from "@exodus/schemasafe"
+import jsonLogic from "json-logic-js"
 
-export const getDoc = (data, path, _signer) => {
-  const [col, id] = path
-  data[col] ||= { __docs: {} }
-  data[col].__docs[id] ||= { __data: null, subs: {} }
-  if (!isNil(_signer) && isNil(data[col].__docs[id].setter)) {
-    data[col].__docs[id].setter = _signer
+export const mergeData = (_data, new_data, overwrite = false) => {
+  if (isNil(_data.__data) || overwrite) _data.__data = {}
+  for (let k in new_data) {
+    const d = new_data[k]
+
+    if (is(Object)(d) && d.__op === "arrayUnion") {
+      if (complement(is)(Array, d.arr)) err()
+      if (complement(is)(Array, _data.__data[k])) _data.__data[k] = []
+      _data.__data[k] = concat(_data.__data[k], d.arr)
+    } else if (is(Object)(d) && d.__op === "arrayRemove") {
+      if (complement(is)(Array, d.arr)) err()
+      if (complement(is)(Array, _data.__data[k])) _data.__data[k] = []
+      _data.__data[k] = without(d.arr, _data.__data[k])
+    } else if (is(Object)(d) && d.__op === "inc") {
+      if (isNaN(d.n)) err()
+      if (isNil(_data.__data[k])) _data.__data[k] = 0
+      _data.__data[k] += d.n
+    } else if (is(Object)(d) && d.__op === "del") {
+      delete _data.__data[k]
+    } else if (is(Object)(d) && d.__op === "ts") {
+      _data.__data[k] = SmartWeave.block.timestamp
+    } else {
+      _data.__data[k] = d
+    }
   }
+  return _data
+}
+
+export const getDoc = (data, path, _signer, func, new_data) => {
+  const [_col, id] = path
+  data[_col] ||= { __docs: {} }
+  const col = data[_col]
+  const { rules, schema } = col
+  col.__docs[id] ||= { __data: null, subs: {} }
+  const doc = col.__docs[id]
+  if (!isNil(_signer) && isNil(doc.setter)) doc.setter = _signer
+  if (
+    includes(func)(["set", "add", "update", "upsert", "delete"]) &&
+    !isNil(rules)
+  ) {
+    let op = func
+    if (includes(op)("set", "add")) op = "create"
+    if (op === "create" && !isNil(doc.__data)) op = "update"
+    if (op === "upsert") {
+      if (!isNil(doc.__data)) {
+        op = "update"
+      } else {
+        op = "create"
+      }
+    }
+    let allowed = false
+    let newData = null
+    if (path.length === 2) {
+      if (includes(func)(["set", "add"])) {
+        newData = mergeData(clone(doc), new_data, true).__data
+      } else if (includes(func)(["update", "upsert"])) {
+        newData = mergeData(clone(doc), new_data).__data
+      }
+    }
+    for (let k in rules) {
+      const rule = rules[k]
+      const [permission, _ops] = k.split(" ")
+      const ops = _ops.split(",")
+      if (intersection(ops)(["write", op]).length > 0) {
+        const rule_data = {
+          request: {
+            auth: { signer: _signer },
+            block: {
+              height: SmartWeave.block.height,
+              timestamp: SmartWeave.block.timestamp,
+            },
+            transaction: {
+              id: SmartWeave.transaction.id,
+            },
+            resource: { data: new_data },
+          },
+          resource: { data: doc.__data, setter: doc.setter, newData },
+        }
+        const ok = jsonLogic.apply(rule, rule_data)
+        if (permission === "allow" && ok) {
+          allowed = true
+        } else if (permission === "deny" && ok) err()
+      }
+    }
+    if (!allowed) err()
+  }
+
   return path.length >= 4
-    ? getDoc(data[col].__docs[id].subs, slice(2, path.length, path), _signer)
-    : { doc: data[col].__docs[id], schema: data[col].schema }
+    ? getDoc(doc.subs, slice(2, path.length, path), _signer, func)
+    : {
+        doc,
+        schema,
+        rules,
+      }
 }
 
 export const getCol = (data, path, _signer) => {
@@ -68,7 +163,7 @@ export const parse = async (state, action, func, signer) => {
   const { query } = action.input
   let new_data = null
   let path = null
-  if (includes(func)(["delete", "getSchema"])) {
+  if (includes(func)(["delete", "getSchema", "getRules"])) {
     path = query
   } else {
     ;[new_data, ...path] = query
@@ -82,26 +177,32 @@ export const parse = async (state, action, func, signer) => {
     }
   }
   if (
-    (isNil(new_data) && !includes(func)(["delete", "getSchema"])) ||
+    (isNil(new_data) && !includes(func)(["delete", "getSchema", "getRules"])) ||
     path.length === 0 ||
-    (path.length % 2 !== 0 && !includes(func)(["setSchema", "getSchema"]))
+    (path.length % 2 !== 0 &&
+      !includes(func)(["setSchema", "getSchema", "setRules", "getRules"]))
   ) {
     err()
   }
   let _data = null
   let schema = null
-  if (includes(func)(["setSchema", "getSchema"])) {
+  let rules = null
+  if (includes(func)(["setSchema", "getSchema", "setRules", "getRules"])) {
     _data = getCol(data, path, signer, func)
   } else {
-    const doc = getDoc(data, path, signer, func)
+    const doc = getDoc(data, path, signer, func, new_data)
     _data = doc.doc
     schema = doc.schema
+    rules = doc.rules
   }
   if (
     includes(func)(["update", "upsert", "delete"]) &&
     _data.setter !== signer
   ) {
-  } else if (func === "setSchema" && action.caller !== state.owner) {
+  } else if (
+    includes(func)(["setSchema", "setRules"]) &&
+    action.caller !== state.owner
+  ) {
     err("caller is not contract owner")
   }
   if (includes(func)(["set", "add", "update", "upsert"]) && !isNil(schema)) {
@@ -109,34 +210,6 @@ export const parse = async (state, action, func, signer) => {
     if (!_validate(new_data)) err()
   }
   return { data, query, new_data, path, _data, schema }
-}
-
-export const mergeData = (_data, new_data, overwrite = false) => {
-  if (isNil(_data.__data) || overwrite) _data.__data = {}
-  for (let k in new_data) {
-    const d = new_data[k]
-
-    if (is(Object)(d) && d.__op === "arrayUnion") {
-      if (complement(is)(Array, d.arr)) err()
-      if (complement(is)(Array, _data.__data[k])) _data.__data[k] = []
-      _data.__data[k] = concat(_data.__data[k], d.arr)
-    } else if (is(Object)(d) && d.__op === "arrayRemove") {
-      if (complement(is)(Array, d.arr)) err()
-      if (complement(is)(Array, _data.__data[k])) _data.__data[k] = []
-      _data.__data[k] = without(d.arr, _data.__data[k])
-    } else if (is(Object)(d) && d.__op === "inc") {
-      if (isNaN(d.n)) err()
-      if (isNil(_data.__data[k])) _data.__data[k] = 0
-      _data.__data[k] += d.n
-    } else if (is(Object)(d) && d.__op === "del") {
-      delete _data.__data[k]
-    } else if (is(Object)(d) && d.__op === "ts") {
-      _data.__data[k] = SmartWeave.block.timestamp
-    } else {
-      _data.__data[k] = d
-    }
-  }
-  return _data
 }
 
 export const err = (msg = `The wrong query`) => {
