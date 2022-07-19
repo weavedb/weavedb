3b031a8de17ad93b47bc19523ff9b1c98ec42c08
Tomoya Nagasawa <ocrybit@gmail.com>
2022-07-19 23:10:32 +0400

    delegate call with address linking

---
+++ src/contracts/actions/write/removeAddressLink.js
@@ -0,0 +1,12 @@
+import { isNil } from "ramda"
+import { err } from "../../lib/utils"
+import { validate } from "../../lib/validate"
+const { recoverTypedSignature } = require("../../lib/eth-sig-util")
+
+export const removeAddressLink = async (state, action, signer) => {
+  signer ||= validate(state, action, "removeAddressLink")
+  const { address } = action.input.query
+  if (isNil(state.auth.links[address])) err("link doesn't exist")
+  delete state.auth.links[address]
+  return { state }
+}
