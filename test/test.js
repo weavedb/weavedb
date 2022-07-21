95412dcbe429042c70582763df9868ff92039254
Tomoya Nagasawa <ocrybit@gmail.com>
2022-07-21 09:50:20 +0400

    factor out sdk

--- test/test.js
+++ test/test.js
@@ -6,45 +6,15 @@ const path = require("path")
 const { expect } = require("chai")
 const { isNil, range } = require("ramda")
 const ethSigUtil = require("@metamask/eth-sig-util")
-const {
-  init,
-  initBeforeEach,
-  addFunds,
-  mineBlock,
-  query,
-  get,
-  cget,
-  getSchema,
-  getRules,
-  getIds,
-  getNonce,
-  getIndexes,
-} = require("./util")
-
-const op = {
-  ts: () => ({ __op: "ts" }),
-  del: () => ({ __op: "del" }),
-  inc: n => ({ __op: "inc", n }),
-  union: (...args) => ({ __op: "arrayUnion", arr: args }),
-  remove: (...args) => ({ __op: "arrayRemove", arr: args }),
-}
+const { init, initBeforeEach, addFunds } = require("./util")
 
 describe("WeaveDB", function () {
-  let arlocal,
-    arweave,
-    warp,
-    wallet,
-    walletAddress,
-    contractSrc,
-    initialState,
-    wdb,
-    domain,
-    wallet2
+  let arlocal, wallet, walletAddress, wallet2, sdk
 
   this.timeout(0)
 
   before(async () => {
-    ;({ arlocal, arweave, warp } = await init())
+    ;({ arlocal, sdk } = await init())
   })
 
   after(async () => {
@@ -52,101 +22,90 @@ describe("WeaveDB", function () {
   })
 
   beforeEach(async () => {
-    ;({
-      wallet,
-      walletAddress,
-      contractSrc,
-      initialState,
-      wdb,
-      domain,
-      wallet,
-      wallet2,
-    } = await initBeforeEach())
+    ;({ walletAddress, wallet, wallet2 } = await initBeforeEach())
   })
 
   it("should get nonce", async () => {
-    expect(await getNonce(wallet.getAddressString())).to.equal(1)
-    await query(wallet, "set", [{ id: 1 }, "col", "doc"])
-    expect(await getNonce(wallet.getAddressString())).to.equal(2)
+    expect(await sdk.getNonce(wallet.getAddressString())).to.equal(1)
+    await sdk.set({ id: 1 }, "col", "doc")
+    expect(await sdk.getNonce(wallet.getAddressString())).to.equal(2)
   })
 
   it("should add & get", async () => {
     const data = { name: "Bob", age: 20 }
-    const tx = await query(wallet, "add", [data, "ppl"])
-    expect(await get(["ppl", (await getIds(tx))[0]])).to.eql(data)
+    const tx = await sdk.add(data, "ppl")
+    expect(await sdk.get("ppl", (await sdk.getIds(tx))[0])).to.eql(data)
   })
 
   it("should set & get", async () => {
     const data = { name: "Bob", age: 20 }
     const data2 = { name: "Alice", height: 160 }
-    await query(wallet, "set", [data, "ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql(data)
-    await query(wallet, "set", [data2, "ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql(data2)
+    await sdk.set(data, "ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql(data)
+    await sdk.set(data2, "ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql(data2)
   })
 
   it("should cget & pagenate", async () => {
     const data = { name: "Bob", age: 20 }
     const data2 = { name: "Alice", age: 160 }
-    await query(wallet, "set", [data, "ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql(data)
-    await query(wallet, "set", [data2, "ppl", "Alice"])
-    const cursor = (await cget(["ppl", ["age"], 1]))[0]
-    expect(await get(["ppl", ["age"], ["startAfter", cursor]])).to.eql([data2])
+    await sdk.set(data, "ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql(data)
+    await sdk.set(data2, "ppl", "Alice")
+    const cursor = (await sdk.cget("ppl", ["age"], 1))[0]
+    expect(await sdk.get("ppl", ["age"], ["startAfter", cursor])).to.eql([
+      data2,
+    ])
   })
 
   it("should update", async () => {
     const data = { name: "Bob", age: 20 }
-    await query(wallet, "set", [data, "ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql(data)
-    await query(wallet, "update", [{ age: 25 }, "ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql({ name: "Bob", age: 25 })
-    await query(wallet, "update", [{ age: op.inc(5) }, "ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql({ name: "Bob", age: 30 })
-    await query(wallet, "update", [{ age: op.del(5) }, "ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql({ name: "Bob" })
+    await sdk.set(data, "ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql(data)
+    await sdk.update({ age: 25 }, "ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql({ name: "Bob", age: 25 })
+    await sdk.update({ age: sdk.inc(5) }, "ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql({ name: "Bob", age: 30 })
+    await sdk.update({ age: sdk.del(5) }, "ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql({ name: "Bob" })
 
     // arrayUnion
-    await query(wallet, "update", [
-      { foods: op.union("pasta", "cake", "wine") },
+    await sdk.update(
+      { foods: sdk.union("pasta", "cake", "wine") },
       "ppl",
-      "Bob",
-    ])
-    expect(await get(["ppl", "Bob"])).to.eql({
+      "Bob"
+    )
+    expect(await sdk.get("ppl", "Bob")).to.eql({
       name: "Bob",
       foods: ["pasta", "cake", "wine"],
     })
 
     // arrayRemove
-    await query(wallet, "update", [
-      { foods: op.remove("pasta", "cake") },
-      "ppl",
-      "Bob",
-    ])
-    expect(await get(["ppl", "Bob"])).to.eql({
+    await sdk.update({ foods: sdk.remove("pasta", "cake") }, "ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql({
       name: "Bob",
       foods: ["wine"],
     })
 
     // timestamp
-    const tx = await query(wallet, "update", [{ death: op.ts() }, "ppl", "Bob"])
-    const tx_data = await arweave.transactions.get(tx)
-    const timestamp = (await arweave.blocks.get(tx_data.block)).timestamp
-    expect((await get(["ppl", "Bob"])).death).to.be.lte(timestamp)
+    const tx = await sdk.update({ death: sdk.ts() }, "ppl", "Bob")
+    const tx_data = await sdk.arweave.transactions.get(tx)
+    const timestamp = (await sdk.arweave.blocks.get(tx_data.block)).timestamp
+    expect((await sdk.get("ppl", "Bob")).death).to.be.lte(timestamp)
   })
 
   it("should upsert", async () => {
     const data = { name: "Bob", age: 20 }
-    await query(wallet, "upsert", [data, "ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql(data)
+    await sdk.upsert(data, "ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql(data)
   })
 
   it("should delete", async () => {
     const data = { name: "Bob", age: 20 }
-    await query(wallet, "set", [data, "ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql(data)
-    await query(wallet, "delete", ["ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql(null)
+    await sdk.set(data, "ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql(data)
+    await sdk.delete("ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql(null)
   })
 
   it("should get a collection", async () => {
@@ -178,20 +137,20 @@ describe("WeaveDB", function () {
       weight: 70,
       letters: ["b", "e", "t", "h"],
     }
-    await query(wallet, "set", [Bob, "ppl", "Bob"])
-    await query(wallet, "set", [Alice, "ppl", "Alice"])
-    await query(wallet, "set", [John, "ppl", "John"])
-    await query(wallet, "set", [Beth, "ppl", "Beth"])
-    expect(await get(["ppl"])).to.eql([Bob, Alice, John, Beth])
+    await sdk.set(Bob, "ppl", "Bob")
+    await sdk.set(Alice, "ppl", "Alice")
+    await sdk.set(John, "ppl", "John")
+    await sdk.set(Beth, "ppl", "Beth")
+    expect(await sdk.get("ppl")).to.eql([Bob, Alice, John, Beth])
 
     // limit
-    expect((await get(["ppl", 1])).length).to.eql(1)
+    expect((await sdk.get("ppl", 1)).length).to.eql(1)
 
     // sort
-    expect(await get(["ppl", ["height"]])).to.eql([Alice, Beth, Bob, John])
+    expect(await sdk.get("ppl", ["height"])).to.eql([Alice, Beth, Bob, John])
 
     // sort desc
-    expect(await get(["ppl", ["height", "desc"]])).to.eql([
+    expect(await sdk.get("ppl", ["height", "desc"])).to.eql([
       John,
       Bob,
       Beth,
@@ -199,99 +158,99 @@ describe("WeaveDB", function () {
     ])
 
     // sort multiple fields
-    await query(wallet, "addIndex", [[["age"], ["weight", "desc"]], "ppl"])
-    expect(await get(["ppl", ["age"], ["weight", "desc"]])).to.eql([
+    await sdk.addIndex([["age"], ["weight", "desc"]], "ppl")
+    expect(await sdk.get("ppl", ["age"], ["weight", "desc"])).to.eql([
       Bob,
       Beth,
       Alice,
       John,
     ])
     // where =
-    expect(await get(["ppl", ["age", "=", 30]])).to.eql([Alice, Beth])
+    expect(await sdk.get("ppl", ["age", "=", 30])).to.eql([Alice, Beth])
 
     // where >
-    expect(await get(["ppl", ["age"], ["age", ">", 30]])).to.eql([John])
+    expect(await sdk.get("ppl", ["age"], ["age", ">", 30])).to.eql([John])
 
     // where >=
-    expect(await get(["ppl", ["age"], ["age", ">=", 30]])).to.eql([
+    expect(await sdk.get("ppl", ["age"], ["age", ">=", 30])).to.eql([
       Beth,
       Alice,
       John,
     ])
 
     // where <
-    expect(await get(["ppl", ["age"], ["age", "<", 30]])).to.eql([Bob])
+    expect(await sdk.get("ppl", ["age"], ["age", "<", 30])).to.eql([Bob])
 
     // where <=
-    expect(await get(["ppl", ["age"], ["age", "<=", 30]])).to.eql([
+    expect(await sdk.get("ppl", ["age"], ["age", "<=", 30])).to.eql([
       Bob,
       Beth,
       Alice,
     ])
 
     // where =!
-    expect(await get(["ppl", ["age"], ["age", "!=", 30]])).to.eql([Bob, John])
+    expect(await sdk.get("ppl", ["age"], ["age", "!=", 30])).to.eql([Bob, John])
 
     // where in
-    expect(await get(["ppl", ["age", "in", [20, 30]]])).to.eql([
+    expect(await sdk.get("ppl", ["age", "in", [20, 30]])).to.eql([
       Bob,
       Alice,
       Beth,
     ])
+
     // where not-in
-    expect(await get(["ppl", ["age"], ["age", "not-in", [20, 30]]])).to.eql([
+    expect(await sdk.get("ppl", ["age"], ["age", "not-in", [20, 30]])).to.eql([
       John,
     ])
 
     // where array-contains
-    expect(await get(["ppl", ["letters", "array-contains", "b"]])).to.eql([
+    expect(await sdk.get("ppl", ["letters", "array-contains", "b"])).to.eql([
       Bob,
       Beth,
     ])
 
     // where array-contains-any
     expect(
-      await get(["ppl", ["letters", "array-contains-any", ["j", "t"]]])
+      await sdk.get("ppl", ["letters", "array-contains-any", ["j", "t"]])
     ).to.eql([John, Beth])
 
     // skip startAt
-    expect(await get(["ppl", ["age"], ["startAt", 30]])).to.eql([
+    expect(await sdk.get("ppl", ["age"], ["startAt", 30])).to.eql([
       Beth,
       Alice,
       John,
     ])
 
     // skip startAfter
-    expect(await get(["ppl", ["age"], ["startAfter", 30]])).to.eql([John])
+    expect(await sdk.get("ppl", ["age"], ["startAfter", 30])).to.eql([John])
 
     // skip endAt
-    expect(await get(["ppl", ["age"], ["endAt", 30]])).to.eql([
+    expect(await sdk.get("ppl", ["age"], ["endAt", 30])).to.eql([
       Bob,
       Beth,
       Alice,
     ])
 
     // skip endBefore
-    expect(await get(["ppl", ["age"], ["endBefore", 30]])).to.eql([Bob])
+    expect(await sdk.get("ppl", ["age"], ["endBefore", 30])).to.eql([Bob])
 
     // skip startAt multiple fields
-    await query(wallet, "addIndex", [[["age"], ["weight"]], "ppl"])
+    await sdk.addIndex([["age"], ["weight"]], "ppl")
     expect(
-      await get(["ppl", ["age"], ["weight"], ["startAt", 30, 70]])
+      await sdk.get("ppl", ["age"], ["weight"], ["startAt", 30, 70])
     ).to.eql([Beth, John])
 
     // skip endAt multiple fields
-    expect(await get(["ppl", ["age"], ["weight"], ["endAt", 30, 60]])).to.eql([
-      Bob,
-      Alice,
-    ])
+    expect(
+      await sdk.get("ppl", ["age"], ["weight"], ["endAt", 30, 60])
+    ).to.eql([Bob, Alice])
   })
 
   it("should batch execute", async () => {
     const data = { name: "Bob", age: 20 }
     const data2 = { name: "Alice", age: 40 }
     const data3 = { name: "Beth", age: 10 }
-    const tx = await query(wallet, "batch", [
+    const tx = await sdk.batch([
       ["set", data, "ppl", "Bob"],
       ["set", data3, "ppl", "Beth"],
       ["update", { age: 30 }, "ppl", "Bob"],
@@ -299,9 +258,9 @@ describe("WeaveDB", function () {
       ["add", data2, "ppl"],
       ["delete", "ppl", "Beth"],
     ])
-    expect(await get(["ppl", "Bob"])).to.eql({ name: "Bob", age: 20 })
-    expect(await get(["ppl", (await getIds(tx))[0]])).to.eql(data2)
-    expect(await get(["ppl", "Beth"])).to.eql(null)
+    expect(await sdk.get("ppl", "Bob")).to.eql({ name: "Bob", age: 20 })
+    expect(await sdk.get("ppl", (await sdk.getIds(tx))[0])).to.eql(data2)
+    expect(await sdk.get("ppl", "Beth")).to.eql(null)
   })
 
   it("should set schema", async () => {
@@ -324,14 +283,14 @@ describe("WeaveDB", function () {
         },
       },
     }
-    await query(wallet, "setSchema", [schema, "ppl"])
-    expect(await getSchema(["ppl"])).to.eql(schema)
-    await query(wallet, "set", [data, "ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql(null)
-    await query(wallet, "setSchema", [schema2, "ppl"])
-    expect(await getSchema(["ppl"])).to.eql(schema2)
-    await query(wallet, "set", [data, "ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql(data)
+    await sdk.setSchema(schema, "ppl")
+    expect(await sdk.getSchema("ppl")).to.eql(schema)
+    await sdk.set(data, "ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql(null)
+    await sdk.setSchema(schema2, "ppl")
+    expect(await sdk.getSchema("ppl")).to.eql(schema2)
+    await sdk.set(data, "ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql(data)
   })
 
   it("should set rules", async () => {
@@ -345,16 +304,16 @@ describe("WeaveDB", function () {
       },
       "deny delete": { "!=": [{ var: "request.auth.signer" }, null] },
     }
-    await query(wallet, "setRules", [rules, "ppl"])
-    expect(await getRules(["ppl"])).to.eql(rules)
-    await query(wallet, "set", [data, "ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql(data)
-    await query(wallet, "delete", ["ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql(data)
-    await query(wallet, "update", [{ age: op.inc(10) }, "ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql({ name: "Bob", age: 20 })
-    await query(wallet, "update", [{ age: op.inc(5) }, "ppl", "Bob"])
-    expect(await get(["ppl", "Bob"])).to.eql({ name: "Bob", age: 25 })
+    await sdk.setRules(rules, "ppl")
+    expect(await sdk.getRules("ppl")).to.eql(rules)
+    await sdk.set(data, "ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql(data)
+    await sdk.delete("ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql(data)
+    await sdk.update({ age: sdk.inc(10) }, "ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql({ name: "Bob", age: 20 })
+    await sdk.update({ age: sdk.inc(5) }, "ppl", "Bob")
+    expect(await sdk.get("ppl", "Bob")).to.eql({ name: "Bob", age: 25 })
   })
 
   it("should add index", async () => {
@@ -362,39 +321,33 @@ describe("WeaveDB", function () {
     const data2 = { name: "Alice", age: 25 }
     const data3 = { name: "Beth", age: 5 }
     const data4 = { name: "John", age: 20, height: 150 }
-    await query(wallet, "add", [data, "ppl"])
-    expect(await get(["ppl", ["age"]])).to.eql([data])
-    await query(wallet, "set", [data2, "ppl", "Alice"])
-    expect(await get(["ppl", ["age", "desc"]])).to.eql([data2, data])
-    await query(wallet, "upsert", [data3, "ppl", "Beth"])
-    expect(await get(["ppl", ["age", "desc"]])).to.eql([data2, data, data3])
-    await query(wallet, "update", [{ age: 30 }, "ppl", "Beth"])
-    expect(await get(["ppl", ["age", "desc"]])).to.eql([
+    await sdk.add(data, "ppl")
+    expect(await sdk.get("ppl", ["age"])).to.eql([data])
+    await sdk.set(data2, "ppl", "Alice")
+    expect(await sdk.get("ppl", ["age", "desc"])).to.eql([data2, data])
+    await sdk.upsert(data3, "ppl", "Beth")
+    expect(await sdk.get("ppl", ["age", "desc"])).to.eql([data2, data, data3])
+    await sdk.update({ age: 30 }, "ppl", "Beth")
+    expect(await sdk.get("ppl", ["age", "desc"])).to.eql([
       { name: "Beth", age: 30 },
       data2,
       data,
     ])
-    await query(wallet, "addIndex", [[["age"], ["name", "desc"]], "ppl"])
-    await query(wallet, "addIndex", [
-      [["age"], ["name", "desc"], ["height"]],
-      "ppl",
-    ])
-    await query(wallet, "addIndex", [
-      [["age"], ["name", "desc"], ["height", "desc"]],
-      "ppl",
-    ])
+    await sdk.addIndex([["age"], ["name", "desc"]], "ppl")
+    await sdk.addIndex([["age"], ["name", "desc"], ["height"]], "ppl")
+    await sdk.addIndex([["age"], ["name", "desc"], ["height", "desc"]], "ppl")
 
-    await query(wallet, "upsert", [data4, "ppl", "John"])
-    expect(await get(["ppl", ["age"], ["name", "desc"]])).to.eql([
+    await sdk.upsert(data4, "ppl", "John")
+    expect(await sdk.get("ppl", ["age"], ["name", "desc"])).to.eql([
       data4,
       data,
       data2,
       { name: "Beth", age: 30 },
     ])
     expect(
-      await get(["ppl", ["age"], ["name", "in", ["Alice", "John"]]])
+      await sdk.get("ppl", ["age"], ["name", "in", ["Alice", "John"]])
     ).to.eql([data4, data2])
-    expect(await getIndexes(["ppl"])).to.eql([
+    expect(await sdk.getIndexes("ppl")).to.eql([
       [["name", "asc"]],
       [["age", "asc"]],
       [
@@ -414,7 +367,7 @@ describe("WeaveDB", function () {
       [["height", "asc"]],
     ])
   })
-  it.only("should link temporarily generated address", async () => {
+  it("should link temporarily generated address", async () => {
     const addr = wallet.getAddressString()
     const identity = EthCrypto.createIdentity()
     const EIP712Domain = [
@@ -422,7 +375,7 @@ describe("WeaveDB", function () {
       { name: "version", type: "string" },
       { name: "verifyingContract", type: "string" },
     ]
-    let nonce = await getNonce(addr)
+    let nonce = await sdk.getNonce(addr)
     const message = {
       nonce,
       query: JSON.stringify({ func: "auth", query: { address: addr } }),
@@ -435,7 +388,7 @@ describe("WeaveDB", function () {
           { name: "nonce", type: "uint256" },
         ],
       },
-      domain,
+      domain: sdk.domain,
       primaryType: "Query",
       message,
     }
@@ -452,29 +405,21 @@ describe("WeaveDB", function () {
         signature,
       })
     ).to.equal(identity.address.toLowerCase())
-    await query(wallet, "addAddressLink", query2, nonce)
-    await query(
-      wallet,
-      "set",
-      [{ name: "Beth", age: 10 }, "ppl", "Beth"],
-      null,
-      identity.address.toLowerCase(),
-      Buffer.from(identity.privateKey.replace(/^0x/, ""), "hex")
-    )
-    expect((await cget(["ppl", "Beth"])).setter).to.eql(addr)
-    await query(wallet, "removeAddressLink", {
+    await sdk.addAddressLink(query2, { nonce })
+    await sdk.set({ name: "Beth", age: 10 }, "ppl", "Beth", {
+      addr: identity.address.toLowerCase(),
+      privateKey: Buffer.from(identity.privateKey.replace(/^0x/, ""), "hex"),
+    })
+    expect((await sdk.cget("ppl", "Beth")).setter).to.eql(addr)
+    await sdk.removeAddressLink({
       address: identity.address.toLowerCase(),
     })
-    await query(
-      wallet,
-      "set",
-      [{ name: "Bob", age: 20 }, "ppl", "Bob"],
-      null,
-      identity.address.toLowerCase(),
-      Buffer.from(identity.privateKey.replace(/^0x/, ""), "hex"),
-      true
-    )
-    expect((await cget(["ppl", "Bob"])).setter).to.eql(
+    await sdk.set({ name: "Bob", age: 20 }, "ppl", "Bob", {
+      addr: identity.address.toLowerCase(),
+      privateKey: Buffer.from(identity.privateKey.replace(/^0x/, ""), "hex"),
+      overwrite: true,
+    })
+    expect((await sdk.cget("ppl", "Bob")).setter).to.eql(
       identity.address.toLowerCase()
     )
   })
