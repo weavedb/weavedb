b814d02274e20a11576d2d74af0b0711bcfa5b68
Tomoya Nagasawa <ocrybit@gmail.com>
2022-09-05 21:51:40 +0400

    integrate internet identity

--- src/contracts/actions/write/removeAddressLink.js
+++ src/contracts/actions/write/removeAddressLink.js
@@ -7,6 +7,12 @@ export const removeAddressLink = async (state, action, signer) => {
   signer ||= validate(state, action, "removeAddressLink")
   const { address } = action.input.query
   if (isNil(state.auth.links[address.toLowerCase()])) err("link doesn't exist")
+  if (
+    signer !== address.toLowerCase() &&
+    signer !== state.auth.links[address.toLowerCase()]
+  ) {
+    err("signer is neither owner nor delegator")
+  }
   delete state.auth.links[address.toLowerCase()]
   return { state }
 }
