f0fc38342f448c351fcc8cacfba703d00a62d8c6
Tomoya Nagasawa <ocrybit@gmail.com>
2022-12-01 12:23:40 +0400

    update logo

---
+++ console/pages/_document.js
@@ -0,0 +1,23 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
+        />
+        <link
+          key="fontawesome"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
