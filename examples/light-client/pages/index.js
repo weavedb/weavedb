74e6e80d9c46e674fe3000bcdd5de12d9538e4a2
Tomoya Nagasawa <ocrybit@gmail.com>
2022-12-01 09:40:10 +0400

    upgrade light-client

--- examples/light-client/pages/index.js
+++ examples/light-client/pages/index.js
@@ -9,12 +9,11 @@ export default function Home() {
       const db = new client({
         name: "weavedb",
         version: "1",
-        contractTxId: "2ohyMxM2Z2exV4dVLgRNa9jMnEY09H_I-5WkkZBR0Ns",
+        contractTxId: "7VqqosbKTwK7A1oeb4BNu2Aou--o5csFn4uzzN9fMz0",
         rpc: "https://grpc.asteroid.ac",
       })
-      setPosts(await db.get("wall", 5, ["date", "desc"]))
+      setPosts(await db.get("wall", ["date", "desc"], 5))
     })()
   }, [])
-
   return <div>{JSON.stringify(posts)}</div>
 }
