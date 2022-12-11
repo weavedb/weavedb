b2a2a0d14a1891c0813037097527d092c5c51958
Tomoya Nagasawa <ocrybit@gmail.com>
2022-12-12 00:04:43 +0400

    [sdk] on with localhost

--- sdk/index.js
+++ sdk/index.js
@@ -12,44 +12,53 @@ let dbs = {}
 let subs = {}
 let submap = {}
 
-const _on = async (contractTxId, block = {}) => {
-  try {
-    let data = await dbs[contractTxId].readState()
-    const state = data.cachedValue.state
-    if (!isNil(state)) {
-      states[contractTxId] = state
-      for (const cid in subs) {
-        for (const q in subs[cid]) {
-          const query = subs[cid][q].query
-          try {
-            const res = await get(
-              state,
-              {
-                input: { query },
-              },
-              null,
-              { block }
-            )
-            if (!isNil(res)) {
+const _on = async (state, contractTxId, block = {}) => {
+  if (!isNil(state)) {
+    states[contractTxId] = state
+    for (const cid in subs) {
+      for (const q in subs[cid]) {
+        const query = subs[cid][q].query
+        try {
+          const res = await get(
+            state,
+            {
+              input: { query },
+            },
+            null,
+            { block }
+          )
+          if (!isNil(res)) {
+            if (subs[cid][q].height < block.height) {
+              subs[cid][q].height = block.height
               if (!equals(res.result, subs[cid][q].prev)) {
                 for (const k in subs[cid][q].subs) {
-                  if (!isNil(res)) subs[cid][q].subs[k].cb(res.result)
+                  try {
+                    if (!isNil(res)) subs[cid][q].subs[k].cb(res.result)
+                  } catch (e) {
+                    console.log(e)
+                  }
                 }
                 subs[cid][q].prev = res.result
               }
             }
-          } catch (e) {}
+          }
+        } catch (e) {
+          console.log(e)
         }
       }
     }
-  } catch (e) {
-    console.log(e)
   }
 }
 
 class CustomSubscriptionPlugin extends WarpSubscriptionPlugin {
   async process(input) {
-    await _on(this.contractTxId, input.interaction.block)
+    try {
+      let data = await dbs[this.contractTxId].db.readState(
+        input.interaction.block.height
+      )
+      const state = data.cachedValue.state
+      await _on(state, this.contractTxId, input.interaction.block)
+    } catch (e) {}
   }
 }
 
@@ -64,8 +73,6 @@ class SDK extends Base {
     web3,
   }) {
     super()
-    if (isNil(contractTxId)) throw Error("contractTxId missing")
-    this.contractTxId = contractTxId
     this.arweave = Arweave.init(arweave)
     LoggerFactory.INST.logLevel("error")
     if (typeof window === "object") {
@@ -89,28 +96,48 @@ class SDK extends Base {
         isNil(arweave) || isNil(arweave.port) ? 1820 : arweave.port
       )
     } else {
-      if (isNil(web3)) {
-        this.warp = WarpFactory.forMainnet()
-      } else {
-        this.warp = WarpFactory.forMainnet()
-      }
+      this.warp = WarpFactory.forMainnet()
     }
-    this.warp.use(new CustomSubscriptionPlugin(contractTxId, this.warp))
     if (all(complement(isNil))([contractTxId, wallet, name, version])) {
       this.initialize({ contractTxId, wallet, name, version, EthWallet })
     }
   }
 
   initialize({ contractTxId, wallet, name, version, EthWallet }) {
+    this.contractTxId = contractTxId
+    if (isNil(contractTxId)) throw Error("contractTxId missing")
     this.db = this.warp
       .contract(contractTxId)
       .connect(wallet)
       .setEvaluationOptions({
         allowBigInt: true,
       })
-    dbs[contractTxId] = this.db
+    dbs[contractTxId] = this
     this.domain = { name, version, verifyingContract: contractTxId }
     if (!isNil(EthWallet)) this.setEthWallet(EthWallet)
+    if (this.network !== "localhost") {
+      this.warp.use(new CustomSubscriptionPlugin(contractTxId, this.warp))
+    } else {
+      setInterval(() => {
+        this.db
+          .readState()
+          .then(async v => {
+            const state = v.cachedValue.state
+            if (!equals(state, this.state)) {
+              this.state = state
+              const info = await this.arweave.network.getInfo()
+              await _on(state, this.contractTxId, {
+                height: info.height,
+                timestamp: Math.round(Date.now() / 1000),
+                id: info.current,
+              })
+            }
+          })
+          .catch(v => {
+            console.log("readState error")
+          })
+      }, 1000)
+    }
   }
 
   async on(...query) {
@@ -118,10 +145,23 @@ class SDK extends Base {
     const cb = query.pop()
     const hash = md5(JSON.stringify(query))
     const id = shortid()
-    subs[this.contractTxId][hash] ||= { prev: undefined, subs: {}, query }
+    subs[this.contractTxId][hash] ||= {
+      prev: undefined,
+      subs: {},
+      query,
+      height: 0,
+    }
     subs[this.contractTxId][hash].subs[id] = { cb, once: false }
     submap[id] = hash
-    _on(this.contractTxId)
+    this.get(...query).then(v => {
+      if (
+        !isNil(subs[this.contractTxId][hash].subs[id]) &&
+        subs[this.contractTxId][hash].height === 0
+      ) {
+        subs[this.contractTxId][hash].prev = v
+        cb(v)
+      }
+    })
     return () => {
       try {
         delete subs[this.contractTxId][hash].subs[id]
@@ -137,7 +177,7 @@ class SDK extends Base {
   }
 
   async viewState(opt) {
-    let res = await this.db.viewState(opt)
+    let res = await this.db.viewState(opt, 3)
     return res.result
   }
 
