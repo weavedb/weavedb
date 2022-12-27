f29d04438eff90f501eccad62206e2197965dabd
Tomoya Nagasawa <ocrybit@gmail.com>
2022-12-27 13:48:33 +0400

    [console] add fotter

--- console/pages/index.js
+++ console/pages/index.js
@@ -381,7 +381,68 @@ export default inject(
         }
       })()
     }, [doc_path])
-
+    const linkStyle = {
+      fontSize: "16px",
+      display: "block",
+      py: 1,
+      as: "a",
+      target: "_blank",
+      sx: { ":hover": { opacity: 0.75 } },
+    }
+    const Footer = () => (
+      <Flex bg="#333" color="white" p={4} justify="center" flex={1}>
+        <Flex maxW="900px" w="100%">
+          <Box flex={1} py={2} px={6}>
+            <Flex align="center" fontSize="20px" fontWeight="bold" mb={3}>
+              WeaveDB
+            </Flex>
+            <Box {...linkStyle} href="https://weavedb.dev">
+              About
+            </Box>
+            <Box {...linkStyle} href="https://weavedb.mirror.xyz">
+              Mirror Blog
+            </Box>
+            <Box {...linkStyle} href="https://docs.weavedb.dev">
+              Demo Dapps
+            </Box>
+          </Box>
+          <Box flex={1} py={2} px={6}>
+            <Flex align="center" fontSize="20px" fontWeight="bold" mb={3}>
+              Developer
+            </Flex>
+            <Box href="https://docs.weavedb.dev" {...linkStyle}>
+              Documentation
+            </Box>
+            <Box {...linkStyle} href="https://fpjson.weavedb.dev">
+              FPJSON
+            </Box>
+            <Box {...linkStyle} href="https://github.com/weavedb/weavedb">
+              <Box as="i" className="fab fa-github" mr={2} />
+              Github
+            </Box>
+          </Box>
+          <Box flex={1} py={2} px={6}>
+            <Flex align="center" fontSize="20px" fontWeight="bold" mb={3}>
+              Community
+            </Flex>
+            <Box {...linkStyle} href="https://docs.weavedb.dev">
+              <Box as="i" className="fab fa-twitter" mr={2} />
+              Twitter
+            </Box>
+            <Box {...linkStyle} href="https://discord.com/invite/YMe3eqf69M">
+              <Box as="i" className="fab fa-discord" mr={2} />
+              Discord
+            </Box>
+            <Box
+              {...linkStyle}
+              href="https://gitcoin.co/grants/7716/weavedb-decentralized-nosql-database"
+            >
+              Gitcoin Grant
+            </Box>
+          </Box>
+        </Flex>
+      </Flex>
+    )
     return (
       <ChakraProvider>
         <style global jsx>{`
@@ -449,9 +510,9 @@ export default inject(
             <ConnectWallet />
           </Flex>
         </Flex>
-        <Flex justify="center" height="100%" fontSize="12px" pt="50px">
-          <Flex maxW="960px" w="100%" h="100%" direction="column" p={3}>
-            <Flex w="100%" justify="center" mb={4} mt={2}>
+        <Flex align="center" direction="column" fontSize="12px" pt="50px">
+          <Flex maxW="960px" w="100%" direction="column" p={3}>
+            <Flex w="100%" justify="center" my={3}>
               {map(v => {
                 return (
                   <Box
@@ -1360,6 +1421,7 @@ export default inject(
             </Flex>
           </Flex>
         </Flex>
+        <Footer />
         {addCollection !== false ? (
           <Flex
             w="100%"
