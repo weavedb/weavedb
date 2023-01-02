2f13bebba642508aa3999edcd31bbe9096d5758d
Tomoya Nagasawa <ocrybit@gmail.com>
2023-01-02 16:33:19 +0400

    [relayer-nft] add ACL contract

--- examples/relayer-nft/nft-contract/test/NFT.js
+++ examples/relayer-nft/nft-contract/test/NFT.js
@@ -6,12 +6,23 @@ describe("NFT", function () {
     const [owner, otherAccount] = await ethers.getSigners()
     const NFT = await ethers.getContractFactory("NFT")
     const nft = await NFT.deploy()
-    return { nft, owner }
+    const ACL = await ethers.getContractFactory("ACL")
+    const acl = await ACL.deploy(nft.address)
+    return { acl, nft, owner }
   }
+
   it("Should mint", async function () {
     const { nft, owner } = await deploy()
     const tx = await nft.mint()
     await tx.wait()
     expect(await nft.ownerOf(0)).to.equal(owner.address)
+  })
+
+  it("Should validate ownership", async function () {
+    const { acl, nft, owner } = await deploy()
+    const tx = await nft.mint()
+    await tx.wait()
+    expect(await acl.isOwner(owner.address, [3, 4, 0])).to.equal(true)
+    expect(await acl.isOwner(owner.address, [5, 6])).to.equal(false)
   })
 })
