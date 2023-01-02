ba103d195596964a5bf07e29f87fbbbcc305f47c
Tomoya Nagasawa <ocrybit@gmail.com>
2023-01-02 16:33:19 +0400

    [relayer-nft] add ACL contract

---
+++ examples/relayer-nft/nft-contract/scripts/deployACL.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat")
+const nft_address = process.env.NFT_ADDRESS
+if (typeof nft_address === "undefined") {
+  console.log("nft_address missing")
+  process.exit()
+}
+
+async function main() {
+  const ACL = await hre.ethers.getContractFactory("ACL")
+  const acl = await ACL.deploy(nft_address)
+
+  await acl.deployed()
+
+  console.log(`ACL deployed to ${acl.address}`)
+}
+
+main().catch(error => {
+  console.error(error)
+  process.exitCode = 1
+})
