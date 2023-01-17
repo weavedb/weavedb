3a93f71e9c259f5d410b8d0a5a6871e6c560b8dc
Tomoya Nagasawa <ocrybit@gmail.com>
2023-01-17 12:24:43 +0400

    [console] setCanEvolve

--- console/lib/weavedb.js
+++ console/lib/weavedb.js
@@ -561,3 +561,44 @@ export const deployDB = async ({
     return { contractTxId, network, port }
   }
 }
+
+export const _setCanEvolve = async ({
+  val: { value, contractTxId },
+  global,
+  set,
+  fn,
+  conf,
+  get,
+}) => {
+  try {
+    const current = get("temp_current")
+    const identity = isNil(current)
+      ? null
+      : await lf.getItem(`temp_address:${contractTxId}:${current}`)
+    let ii = null
+    if (is(Array)(identity)) {
+      ii = Ed25519KeyIdentity.fromJSON(JSON.stringify(identity))
+    }
+    const opt = !isNil(ii)
+      ? { ii }
+      : !isNil(identity) && !isNil(identity.tx)
+      ? {
+          wallet: current,
+          privateKey: identity.privateKey,
+        }
+      : null
+    if (isNil(opt)) {
+      alert("not logged in")
+      return
+    }
+    const res = await sdk.setCanEvolve(value, opt)
+    if (!isNil(res.err)) {
+      return `Error: ${res.err.errorMessage}`
+    } else {
+      return JSON.stringify(res)
+    }
+  } catch (e) {
+    console.log(e)
+    return `Error: Something went wrong`
+  }
+}
