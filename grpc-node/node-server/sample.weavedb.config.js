141ead9887fc0acee8f31a9c7541414637f1e60f
kaz <kzzz123@protonmail.com>
2023-02-01 08:54:04 +0400

    update how to specify without environment variables.

--- grpc-node/node-server/sample.weavedb.config.js
+++ grpc-node/node-server/sample.weavedb.config.js
@@ -9,9 +9,13 @@ module.exports = {
   // s3: {
   //   bucket: 'BUCKET_NAME',
   //   prefix: 'PREFIX/',
+  //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  //   region: process.env.AWS_REGION,
   // },
   cache: "lmdb", // or "leveldb"
   // redis: {
   //   url: `redis://${process.env.REDISPORT}:${process.env.REDISPORT}`,
   // }
 }
+
