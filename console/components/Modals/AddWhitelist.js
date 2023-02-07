dff88eba44145dae7dc43d333cae21eacff4925b
Tomoya Nagasawa <ocrybit@gmail.com>
2023-02-07 04:41:27 +0400

    [console] abstract AddRelayer

---
+++ console/components/Modals/AddWhitelist.js
@@ -0,0 +1,9 @@
+import { useState } from "react"
+import { Box, Flex } from "@chakra-ui/react"
+import {} from "ramda"
+import { inject } from "roidjs"
+import {} from "../../lib/weavedb"
+
+export default inject(["loading", "temp_current"], ({ fn, set, $ }) => {
+  return null
+})
