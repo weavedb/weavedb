32091c1ba2ef15901bec540206f02cc8f938719c
Tomoya Nagasawa <ocrybit@gmail.com>
2023-02-07 05:22:43 +0400

    [console] abstract AddIndex

--- console/components/Modals.js
+++ console/components/Modals.js
@@ -64,7 +64,7 @@ import AddContract from "./Modals/AddContract"
 import AddInstance from "./Modals/AddInstance"
 import Connect from "./Modals/Connect"
 import AddRules from "./Modals/AddRules"
-
+import AddIndex from "./Modals/AddIndex"
 import AddOwner from "./Modals/AddOwner"
 import AddNodeOwner from "./Modals/AddNodeOwner"
 import AddAlgorithms from "./Modals/AddAlgorithms"
@@ -129,13 +129,11 @@ export default inject(
     setAddSecure,
     addIndex,
     setAddIndex,
-    newIndex,
     indexes,
     setIndexes,
     addInstance,
     port,
     setAddInstance,
-    setNewIndex,
     state,
     setAddEvolve,
     addEvolve,
@@ -305,121 +303,18 @@ export default inject(
       ) : addSecure !== false ? (
         <AddSecure {...{ setAddSecure, state, contractTxId, db, setState }} />
       ) : addIndex !== false ? (
-        <Flex
-          w="100%"
-          h="100%"
-          position="fixed"
-          sx={{ top: 0, left: 0, zIndex: 100, cursor: "pointer" }}
-          bg="rgba(0,0,0,0.5)"
-          onClick={() => setAddIndex(false)}
-          justify="center"
-          align="center"
-        >
-          <Box
-            bg="white"
-            width="500px"
-            p={3}
-            sx={{ borderRadius: "5px", cursor: "default" }}
-            onClick={e => e.stopPropagation()}
-          >
-            <Textarea
-              mt={3}
-              value={newIndex}
-              placeholder="Compound Index"
-              onChange={e => setNewIndex(e.target.value)}
-              sx={{
-                borderRadius: "3px",
-              }}
-            />
-            <Flex
-              mt={4}
-              sx={{
-                borderRadius: "3px",
-                cursor: "pointer",
-                ":hover": { opacity: 0.75 },
-              }}
-              p={2}
-              justify="center"
-              align="center"
-              color="white"
-              bg="#333"
-              height="40px"
-              onClick={async () => {
-                if (isNil($.loading)) {
-                  const exIndex = !/^\s*$/.test(newIndex)
-                  if (!exIndex) {
-                    alert("Enter rules")
-                    return
-                  }
-                  let val = null
-                  let obj
-                  try {
-                    eval(`obj = ${newIndex}`)
-                    if (!is(Array, obj)) {
-                      alert("Index must be an array")
-                      return
-                    }
-                    if (obj.length < 2) {
-                      alert("Compound Index must have at least 2 fields")
-                      return
-                    }
-                    val = newIndex
-                  } catch (e) {
-                    alert("Wrong JSON format")
-                    return
-                  }
-                  const serialize = v =>
-                    map(v2 => {
-                      let v3 = clone(v2)
-                      if (v3.length < 2) v3.push("asc")
-                      return join(":")(v2)
-                    })(v).join(",")
-                  if (
-                    compose(includes(serialize(obj)), map(serialize))(indexes)
-                  ) {
-                    alert("Index exists")
-                    return
-                  }
-                  set("add_index", "loading")
-                  let col_path = compose(
-                    join(", "),
-                    map(v => `"${v}"`),
-                    append(col)
-                  )(base_path)
-                  let query = `${newIndex}, ${col_path}`
-                  const res = JSON.parse(
-                    await fn(queryDB)({
-                      method: "addIndex",
-                      query,
-                      contractTxId,
-                    })
-                  )
-                  if (!res.success) {
-                    alert("Something went wrong")
-                  } else {
-                    setNewIndex("[]")
-                    setAddIndex(false)
-                    setIndexes(
-                      await db.getIndexes(
-                        ...(doc_path.length % 2 === 0
-                          ? doc_path.slice(0, -1)
-                          : doc_path),
-                        true
-                      )
-                    )
-                  }
-                  set(null, "loading")
-                }
-              }}
-            >
-              {!isNil($.loading) ? (
-                <Box as="i" className="fas fa-spin fa-circle-notch" />
-              ) : (
-                "Add"
-              )}
-            </Flex>
-          </Box>
-        </Flex>
+        <AddIndex
+          {...{
+            setAddIndex,
+            indexes,
+            setIndexes,
+            contractTxId,
+            col,
+            base_path,
+            doc_path,
+            db,
+          }}
+        />
       ) : addInstance !== false ? (
         <AddInstance
           {...{
