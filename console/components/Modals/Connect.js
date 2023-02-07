e6eec0d802196d76a8c92c2b081e88abcfc69e39
Tomoya Nagasawa <ocrybit@gmail.com>
2023-02-07 05:50:55 +0400

    [console] abstract ConnectLocal

--- console/components/Modals/Connect.js
+++ console/components/Modals/Connect.js
@@ -20,111 +20,167 @@ export default inject(
   ],
   ({ newNetwork, contractTxId, network, tab, fn, set, $ }) => {
     return (
-      <>
+      <Flex
+        align="center"
+        justify="center"
+        sx={{
+          bg: "rgba(0,0,0,.5)",
+          position: "fixed",
+          w: "100%",
+          h: "100%",
+          zIndex: 100,
+          top: 0,
+          left: 0,
+          cursor: "pointer",
+        }}
+        onClick={() => {
+          set(false, "signing_in_modal")
+          set(false, "owner_signing_in_modal")
+        }}
+      >
         <Flex
-          justify="center"
-          align="center"
-          direction="column"
-          boxSize="150px"
+          width="580px"
+          wrap="wrap"
           p={4}
-          m={4}
-          bg="#333"
-          color="white"
-          sx={{
-            borderRadius: "10px",
-            cursor: "pointer",
-            ":hover": { opacity: 0.75 },
-          }}
-          onClick={async () => {
-            set(true, "signing_in")
-            if ($.owner_signing_in_modal) {
-              await fn(connectAddress)({ network: newNetwork })
-            } else {
-              await fn(createTempAddress)({
-                contractTxId,
-                network,
-                node: tab === "Nodes",
-              })
-            }
-            set(false, "signing_in")
-            set(false, "signing_in_modal")
-            set(false, "owner_signing_in_modal")
-          }}
-        >
-          <Image height="100px" src="/static/images/metamask.png" />
-          <Box textAlign="center">MetaMask</Box>
-        </Flex>
-        <Flex
-          p={4}
-          m={4}
-          boxSize="150px"
-          bg="#333"
-          color="white"
           justify="center"
-          align="center"
-          direction="column"
-          sx={{
-            borderRadius: "10px",
-            cursor: "pointer",
-            ":hover": { opacity: 0.75 },
-          }}
-          onClick={async () => {
-            set(true, "signing_in")
-            if ($.owner_signing_in_modal) {
-              await fn(connectAddressWithII)({
-                network: newNetwork,
-              })
-            } else {
-              await fn(createTempAddressWithII)({
-                contractTxId,
-                network,
-                node: tab === "Nodes",
-              })
-            }
-            set(false, "signing_in")
-            set(false, "signing_in_modal")
-            set(false, "owner_signing_in_modal")
-          }}
+          bg="white"
+          sx={{ borderRadius: "10px", cursor: "default" }}
+          onClick={e => e.stopPropagation()}
         >
-          <Image height="100px" src="/static/images/dfinity.png" />
-          <Box textAlign="center">Internet Identity</Box>
+          {$.signing_in ? (
+            <Flex
+              justify="center"
+              align="center"
+              direction="column"
+              boxSize="150px"
+              p={4}
+              m={4}
+              color="#333"
+              bg="white"
+              sx={{
+                borderRadius: "10px",
+                cursor: "pointer",
+                ":hover": { opacity: 0.75 },
+              }}
+              onClick={async () => set(false, "signing_in")}
+            >
+              <Box
+                fontSize="50px"
+                mb={3}
+                as="i"
+                className="fas fa-spin fa-circle-notch"
+              />
+              <Box textAlign="center">cancel sign-in</Box>
+            </Flex>
+          ) : (
+            <>
+              <Flex
+                justify="center"
+                align="center"
+                direction="column"
+                boxSize="150px"
+                p={4}
+                m={4}
+                bg="#333"
+                color="white"
+                sx={{
+                  borderRadius: "10px",
+                  cursor: "pointer",
+                  ":hover": { opacity: 0.75 },
+                }}
+                onClick={async () => {
+                  set(true, "signing_in")
+                  if ($.owner_signing_in_modal) {
+                    await fn(connectAddress)({ network: newNetwork })
+                  } else {
+                    await fn(createTempAddress)({
+                      contractTxId,
+                      network,
+                      node: tab === "Nodes",
+                    })
+                  }
+                  set(false, "signing_in")
+                  set(false, "signing_in_modal")
+                  set(false, "owner_signing_in_modal")
+                }}
+              >
+                <Image height="100px" src="/static/images/metamask.png" />
+                <Box textAlign="center">MetaMask</Box>
+              </Flex>
+              <Flex
+                p={4}
+                m={4}
+                boxSize="150px"
+                bg="#333"
+                color="white"
+                justify="center"
+                align="center"
+                direction="column"
+                sx={{
+                  borderRadius: "10px",
+                  cursor: "pointer",
+                  ":hover": { opacity: 0.75 },
+                }}
+                onClick={async () => {
+                  set(true, "signing_in")
+                  if ($.owner_signing_in_modal) {
+                    await fn(connectAddressWithII)({
+                      network: newNetwork,
+                    })
+                  } else {
+                    await fn(createTempAddressWithII)({
+                      contractTxId,
+                      network,
+                      node: tab === "Nodes",
+                    })
+                  }
+                  set(false, "signing_in")
+                  set(false, "signing_in_modal")
+                  set(false, "owner_signing_in_modal")
+                }}
+              >
+                <Image height="100px" src="/static/images/dfinity.png" />
+                <Box textAlign="center">Internet Identity</Box>
+              </Flex>
+              <Flex
+                p={4}
+                m={4}
+                boxSize="150px"
+                bg="#333"
+                color="white"
+                justify="center"
+                align="center"
+                direction="column"
+                sx={{
+                  borderRadius: "10px",
+                  cursor: "pointer",
+                  ":hover": { opacity: 0.75 },
+                }}
+                onClick={async () => {
+                  set(true, "signing_in")
+                  if ($.owner_signing_in_modal) {
+                    await fn(connectAddressWithAR)({
+                      network: newNetwork,
+                    })
+                  } else {
+                    await fn(createTempAddressWithAR)({
+                      contractTxId,
+                      network,
+                      node: tab === "Nodes",
+                    })
+                  }
+                  set(false, "signing_in")
+                  set(false, "signing_in_modal")
+                  set(false, "owner_signing_in_modal")
+                }}
+              >
+                <Image height="100px" src="/static/images/arconnect.png" />
+                <Box textAlign="center">ArConnect</Box>
+              </Flex>
+            </>
+          )}
         </Flex>
-        <Flex
-          p={4}
-          m={4}
-          boxSize="150px"
-          bg="#333"
-          color="white"
-          justify="center"
-          align="center"
-          direction="column"
-          sx={{
-            borderRadius: "10px",
-            cursor: "pointer",
-            ":hover": { opacity: 0.75 },
-          }}
-          onClick={async () => {
-            set(true, "signing_in")
-            if ($.owner_signing_in_modal) {
-              await fn(connectAddressWithAR)({
-                network: newNetwork,
-              })
-            } else {
-              await fn(createTempAddressWithAR)({
-                contractTxId,
-                network,
-                node: tab === "Nodes",
-              })
-            }
-            set(false, "signing_in")
-            set(false, "signing_in_modal")
-            set(false, "owner_signing_in_modal")
-          }}
-        >
-          <Image height="100px" src="/static/images/arconnect.png" />
-          <Box textAlign="center">ArConnect</Box>
-        </Flex>
-      </>
+      </Flex>
     )
   }
 )
