45f54b75e16dc9145d10fa859a9ec77c94526ed6
Tomoya Nagasawa <ocrybit@gmail.com>
2023-02-09 23:02:36 +0400

    [console] replace schema textarea with JSON editor

--- console/components/Schemas.js
+++ console/components/Schemas.js
@@ -1,14 +1,18 @@
 import { Box, Flex } from "@chakra-ui/react"
 import { isNil } from "ramda"
 import JSONPretty from "react-json-pretty"
-export default ({ col, schema, setAddSchema }) => (
+
+export default ({ col, schema, setAddSchema, newSchema, setNewSchema }) => (
   <Flex flex={1} sx={{ border: "1px solid #555" }} direction="column">
     <Flex py={2} px={3} color="white" bg="#333" h="35px">
       <Box>Schemas</Box>
       <Box flex={1} />
       {isNil(col) ? null : (
         <Box
-          onClick={() => setAddSchema(true)}
+          onClick={() => {
+            setNewSchema(JSON.stringify(schema))
+            setAddSchema(true)
+          }}
           sx={{
             cursor: "pointer",
             ":hover": { opacity: 0.75 },
