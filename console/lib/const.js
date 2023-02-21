3151fd232c99812233f6cd22768f1d44675a961a
Tomoya Nagasawa <ocrybit@gmail.com>
2023-02-21 15:08:43 +0400

    [contract] upgrade to v0.21.0

--- console/lib/const.js
+++ console/lib/const.js
@@ -103,8 +103,8 @@ export const preset_rpcs = [
   "http://localhost:8080",
 ]
 
-export const latest = "0.20.0"
-export const weavedbSrcTxId = "pfzMiEGWwoyAL33M2ceRUkgG3XvxUyjxUqiyLNHD66g"
+export const latest = "0.21.0"
+export const weavedbSrcTxId = "nNz22bZG_Y2K9r68iqnL1iOPEu8rvqCWNE4TX17OsgA"
 //export const intmaxSrcTxId = "OTfBnNttwsi8b_95peWJ53eJJRqPrVh0s_0V-e5-s94"
 export const dfinitySrcTxId = "3OnjOPuWzB138LOiNxqq2cKby2yANw6RWcQVEkztXX8"
 export const ethereumSrcTxId = "Awwzwvw7qfc58cKS8cG3NsPdDet957-Bf-S1RcHry0w"
