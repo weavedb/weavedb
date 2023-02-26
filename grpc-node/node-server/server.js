8788f5b0a9929b48a3d75a3d69a3ed0a94bfe662
Tomoya Nagasawa <ocrybit@gmail.com>
2023-02-26 15:23:15 +0400

    [node] add ContractManager node

--- grpc-node/node-server/server.js
+++ grpc-node/node-server/server.js
@@ -1,5 +1,11 @@
-const { port = 9090, config = "./weavedb.config.js" } = require("yargs")(
-  process.argv.slice(2)
-).argv
+const {
+  port = 9090,
+  port_manager = 9091,
+  config = "./weavedb.config.js",
+} = require("yargs")(process.argv.slice(2)).argv
 
-new (require("./lib/node").Node)({ conf: require(config), port }).init()
+new (require("./lib/Gateway").Gateway)({
+  conf: require(config),
+  port,
+  port_manager,
+}).init()
