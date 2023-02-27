9318335b72dc7fea361f84296a41f971690909b7
Tomoya Nagasawa <ocrybit@gmail.com>
2023-02-27 08:21:11 +0400

    [sdk-node] use weavedb-contracts

--- sdk/sdk-node/off-chain/actions/read/getRelayerJob.js
+++
@@ -1,6 +0,0 @@
-const getRelayerJob = async (state, action) => {
-  const jobs = state.relayers || {}
-  return { result: jobs[action.input.query[0]] || null }
-}
-
-module.exports = { getRelayerJob }
