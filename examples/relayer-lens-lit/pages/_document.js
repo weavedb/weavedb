6700ef564063558ae292a3d1111edfdeef9fb6dc
Tomoya Nagasawa <ocrybit@gmail.com>
2023-03-11 11:29:11 +0400

    [example] relayer-lens-lit demo

--- examples/relayer-lens-lit/pages/_document.js
+++ examples/relayer-lens-lit/pages/_document.js
@@ -1,9 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {" "}
+        <link
+          key="fontawesome"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
