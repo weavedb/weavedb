d93ed764b02df7ce6527d52450a929476c05d900
Tomoya Nagasawa <ocrybit@gmail.com>
2023-04-09 08:00:32 +0400

    [console] simulate rules

--- console/components/Rules.js
+++ console/components/Rules.js
@@ -2,7 +2,14 @@ import { Box, Flex } from "@chakra-ui/react"
 import { isNil } from "ramda"
 import JSONPretty from "react-json-pretty"
 
-export default ({ rules, isOwner, setNewRules, setAddRules, col }) => (
+export default ({
+  setEditRules,
+  rules,
+  isOwner,
+  setNewRules,
+  setAddRules,
+  col,
+}) => (
   <Flex flex={1} sx={{ border: "1px solid #555" }} direction="column">
     <Flex py={2} px={3} color="white" bg="#333" h="35px">
       <Box>Rules</Box>
@@ -13,7 +20,10 @@ export default ({ rules, isOwner, setNewRules, setAddRules, col }) => (
             if (!isOwner) {
               alert("connect the owner wallet to DB")
             } else {
-              setNewRules(JSON.stringify(rules || { "allow write": true }))
+              setNewRules(
+                JSON.stringify(rules || { "allow write": true }, undefined, 2)
+              )
+              setEditRules(col)
               setAddRules(true)
             }
           }}
