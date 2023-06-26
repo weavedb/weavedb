7a1815dd3a1970bb42f86eab0455467d36eaa9b9
Tomoya Nagasawa <ocrybit@gmail.com>
2023-06-26 20:48:33 +0400

    drop EXM support

--- contracts/common/exm/actions/read/copy.js
+++
@@ -1,8 +0,0 @@
-import { err } from "../../../lib/utils"
-
-export const copy = async (state, action) => {
-  if (state.canEvolve !== true) err("contract cannot evolve")
-  return {
-    result: state,
-  }
-}
