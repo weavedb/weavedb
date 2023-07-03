63075cbf5319d5b246f2a489e626d88b3b5387b4
Tomoya Nagasawa <ocrybit@gmail.com>
2023-07-03 07:40:12 +0400

    [contracts] move shared read files to common

--- sdk/contracts/common/actions/read/getLinkedContract.js
+++ sdk/contracts/common/actions/read/getLinkedContract.js
@@ -1,11 +1,7 @@
-const { isNil } = require("ramda")
-
 const getLinkedContract = async (state, action) => {
   const contracts = state.contracts || {}
   return {
     result: contracts[action.input.query[0]] || null,
   }
 }
-module.exports = {
-  getLinkedContract,
-}
+module.exports = { getLinkedContract }
