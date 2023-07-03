cd83fb6b3c13a3aa92490be8cde082558d838d8b
Tomoya Nagasawa <ocrybit@gmail.com>
2023-07-03 04:15:28 +0400

    [contracts] remove unnecessary files

--- contracts/common/actions/read/version.js
+++
@@ -1,7 +0,0 @@
-import { isNil } from "ramda"
-import { err } from "../../lib/utils"
-
-export const version = async (state, action) => {
-  if (isNil(state.version)) err(`No version assigned`)
-  return { result: state.version }
-}
