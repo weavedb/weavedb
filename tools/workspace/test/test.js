6d866d5cd7e9728e6d52938a898af90449005ca3
Tomoya Nagasawa <ocrybit@gmail.com>
2023-10-14 01:56:23 +0400

    [tools] skelton

---
+++ tools/workspace/test/test.js
@@ -0,0 +1,10 @@
+const config = require("../weavedb.config.js")
+
+describe("WeaveDB", () => {
+  it("should deploy DB contract", () => {})
+  it("should set schemas", () => {})
+  it("should set rules", () => {})
+  it("should set triggers", () => {})
+  it("should set crons", () => {})
+  it("should execute queries", () => {})
+})
