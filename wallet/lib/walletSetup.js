24d660b3ecd39b815a40bd5b10fba5695ed35ca0
Tomoya Nagasawa <ocrybit@gmail.com>
2023-12-26 12:53:33 +0400

    [wallet] encrypt underlying databases

--- wallet/lib/walletSetup.js
+++ wallet/lib/walletSetup.js
@@ -1,8 +1,16 @@
-import { InMemoryDB, Merkletree, str2Bytes } from "@iden3/js-merkletree"
+import { set, get, del, values, createStore } from "idb-keyval"
+import {
+  InMemoryDB,
+  Merkletree,
+  str2Bytes,
+  IndexedDBStorage,
+} from "@iden3/js-merkletree"
 import { proving } from "@iden3/js-jwz"
 import * as uuid from "uuid"
 import lf from "localforage"
+import { map } from "ramda"
 import {
+  MerkleTreeType,
   StorageErrors,
   BjjProvider,
   CredentialStorage,
@@ -15,13 +23,6 @@ import {
   IdentityStorage,
   IdentityWallet,
   IIdentityWallet,
-  InMemoryPrivateKeyStore,
-  LocalStoragePrivateKeyStore,
-  IndexedDBPrivateKeyStore,
-  BrowserDataSource,
-  IndexedDBDataSource,
-  MerkleTreeLocalStorage,
-  MerkleTreeIndexedDBStorage,
   KMS,
   KmsKeyType,
   Profile,
@@ -48,25 +49,26 @@ import {
   PackageManager,
   AgentResolver,
   CircuitStorage,
-  AbstractPrivateKeyStore,
 } from "@0xpolygonid/js-sdk"
-import path from "path"
+
 const rpcUrl = "https://rpc.ankr.com/polygon_mumbai"
 const contractAddress = "0x134B1BE34911E39A8397ec6289782989729807a4"
 const circuitsFolder = "./circuits"
 
-export function initInMemoryDataStorage() {
+export function initIndexedDBDataStorage(aid, aes) {
   let conf = defaultEthConnectionConfig
   conf.contractAddress = contractAddress
   conf.url = rpcUrl
   var dataStorage = {
-    credential: new CredentialStorage(new IndexedDBDataSource("credential")),
-    proof: new CredentialStorage(new IndexedDBDataSource("proof")),
-    identity: new IdentityStorage(
-      new IndexedDBDataSource("identity-1"),
-      new IndexedDBDataSource("identity-2")
+    credential: new CredentialStorage(
+      new IndexedDBDataSource("credential", aid, aes)
     ),
-    mt: new MerkleTreeIndexedDBStorage(40),
+    proof: new CredentialStorage(new IndexedDBDataSource("proof", aid, aes)),
+    identity: new IdentityStorage(
+      new IndexedDBDataSource("identity-1", aid, aes),
+      new IndexedDBDataSource("identity-2", aid, aes)
+    ),
+    mt: new MerkleTreeIndexedDBStorage(40, aid, aes),
     states: new EthStateStorage(defaultEthConnectionConfig),
   }
 
@@ -85,11 +87,10 @@ export async function initIdentityWallet(
   return new IdentityWallet(kms, dataStorage, credentialWallet)
 }
 
-export async function initInMemoryDataStorageAndWallets() {
-  const dataStorage = initInMemoryDataStorage()
+export async function initDataStorageAndWallets(aid, aes) {
+  const dataStorage = initIndexedDBDataStorage(aid, aes)
   const credentialWallet = await initCredentialWallet(dataStorage)
-  const memoryKeyStore = new IndexedDBPrivateKeyStore()
-
+  const memoryKeyStore = new IndexedDBPrivateKeyStore(aid, aes)
   const identityWallet = await initIdentityWallet(
     dataStorage,
     credentialWallet,
@@ -201,68 +202,204 @@ class CStorage {
   }
   async save(circuitId, circuitData) {}
 }
+const enc = new TextEncoder()
+const dec = new TextDecoder()
 
-class InMemoryDataSource {
-  constructor(name) {
-    this.name = name
-    this.isInit = false
-    this.list = []
-    this._data = []
-    this.init()
+const MERKLE_TREE_TYPES = [
+  MerkleTreeType.Claims,
+  MerkleTreeType.Revocations,
+  MerkleTreeType.Roots,
+]
+
+const createMerkleTreeMetaInfo = identifier => {
+  const treesMeta = []
+  for (let index = 0; index < MERKLE_TREE_TYPES.length; index++) {
+    const mType = MERKLE_TREE_TYPES[index]
+    const treeId = `${identifier}+${mType}`
+    treesMeta.push({ treeId, identifier, type: mType })
   }
-  async init() {
-    if (this.isInit === false) {
-      this.isInit = true
-      this._data = []
-      this.list = (await lf.getItem(`list-${this.name}`)) ?? {}
-      for (const v in this.list) {
-        this._data.push(await lf.getItem(`${this.name}-${v}`))
-      }
-      console.log(this.name, this._data)
-    }
+  return treesMeta
+}
+
+const decrypt = async (key, mkey) => {
+  if (!mkey) return mkey
+  const raw = await crypto.subtle.decrypt(
+    { name: "AES-GCM", iv: mkey.iv },
+    key,
+    mkey.val
+  )
+  return JSON.parse(dec.decode(raw))
+}
+const encrypt = async (key, value) => {
+  const msg = enc.encode(JSON.stringify(value))
+  const iv = window.crypto.getRandomValues(new Uint8Array(12))
+  const val = await crypto.subtle.encrypt({ name: "AES-GCM", iv }, key, msg)
+  return { iv, val }
+}
+class IndexedDBDataSource {
+  constructor(_storageKey, aid, key) {
+    this.key = key
+    this.id = aid
+    this._store = createStore(`${_storageKey}-db`, _storageKey)
   }
-  async waitForInit() {
-    return new Promise((res, rej) => {
-      let st = setTimeout(() => {
-        if (this.isInit) {
-          try {
-            st()
-          } catch (e) {}
-          res(this._data)
-        }
-      }, 100)
-    })
-  }
-  /** saves in the memory */
+
   async save(key, value, keyName = "id") {
-    console.log("save...", this.name, key, value)
-    await lf.setItem(`${this.name}-${key}`, value)
-    this.list[key] = true
-    await lf.setItem(`list-${this.name}`, this.list)
-    const itemIndex = this._data.findIndex(i => i[keyName] === key)
-    if (itemIndex === -1) {
-      this._data.push(value)
-    } else {
-      this._data[itemIndex] = value
-    }
+    return this._set(key, value)
+  }
+  async _get(key) {
+    return await decrypt(this.key, await get(key, this._store))
+  }
+  async _set(key, val) {
+    return await set(key, await encrypt(this.key, val), this._store)
   }
 
-  /** gets value from from the memory */
   async get(key, keyName = "id") {
-    return this._data.find(t => t[keyName] === key)
+    return await this._get(key)
   }
 
-  /** loads from value from the memory */
   async load() {
-    return this._data
+    let vals = []
+    for (const mkey of await values(this._store)) {
+      vals.push(await decrypt(this.key, mkey))
+    }
+    return vals
+  }
+  async delete(key, keyName = "id") {
+    return del(key, this._store)
+  }
+}
+
+class MerkleTreeIndexedDBStorage {
+  static storageKeyMeta = "merkle-tree-meta"
+  static storageBindingKeyMeta = "binding-did"
+  constructor(_mtDepth, aid, key) {
+    this._mtDepth = _mtDepth
+    this.key = key
+    this.id = aid
+    this._merkleTreeMetaStore = createStore(
+      `${MerkleTreeIndexedDBStorage.storageKeyMeta}-db`,
+      MerkleTreeIndexedDBStorage.storageKeyMeta
+    )
+    this._bindingStore = createStore(
+      `${MerkleTreeIndexedDBStorage.storageBindingKeyMeta}-db`,
+      MerkleTreeIndexedDBStorage.storageBindingKeyMeta
+    )
+  }
+  async get(key, store) {
+    return await decrypt(this.key, await get(key, store))
+  }
+  async set(key, val, store) {
+    return await set(key, await encrypt(this.key, val), store)
   }
 
-  /** deletes from value from the memory */
-  async delete(key, keyName = "id") {
-    const newData = this._data.filter(i => i[keyName] !== key)
-    if (newData.length === this._data.length) {
-      throw new Error(`${StorageErrors.ItemNotFound} to delete: ${key}`)
+  async createIdentityMerkleTrees(identifier) {
+    if (!identifier) {
+      identifier = `${uuid.v4()}`
     }
-    this._data = newData
+    const existingBinging = await this.get(identifier, this._bindingStore)
+    if (existingBinging) {
+      throw new Error(
+        `Present merkle tree meta information in the store for current identifier ${identifier}`
+      )
+    }
+    const treesMeta = createMerkleTreeMetaInfo(identifier)
+    await this.set(identifier, treesMeta, this._merkleTreeMetaStore)
+    return treesMeta
+  }
+  async getIdentityMerkleTreesInfo(identifier) {
+    const meta = await this.get(identifier, this._merkleTreeMetaStore)
+    if (meta) {
+      return meta
+    }
+    throw new Error(`Merkle tree meta not found for identifier ${identifier}`)
+  }
+
+  async getMerkleTreeByIdentifierAndType(identifier, mtType) {
+    const meta = await this.get(identifier, this._merkleTreeMetaStore)
+    const err = new Error(
+      `Merkle tree not found for identifier ${identifier} and type ${mtType}`
+    )
+    if (!meta) {
+      throw err
+    }
+
+    const resultMeta = meta.find(
+      m => m.identifier === identifier && m.type === mtType
+    )
+    if (!resultMeta) {
+      throw err
+    }
+    return new Merkletree(
+      new IndexedDBStorage(str2Bytes(resultMeta.treeId)),
+      true,
+      this._mtDepth
+    )
+  }
+  async addToMerkleTree(identifier, mtType, hindex, hvalue) {
+    const meta = await this.get(identifier, this._merkleTreeMetaStore)
+    if (!meta) {
+      throw new Error(`Merkle tree meta not found for identifier ${identifier}`)
+    }
+    const resultMeta = meta.find(
+      m => m.identifier === identifier && m.type === mtType
+    )
+    if (!resultMeta) {
+      throw new Error(
+        `Merkle tree not found for identifier ${identifier} and type ${mtType}`
+      )
+    }
+
+    const tree = new Merkletree(
+      new IndexedDBStorage(str2Bytes(resultMeta.treeId)),
+      true,
+      this._mtDepth
+    )
+
+    await tree.add(hindex, hvalue)
+  }
+  async bindMerkleTreeToNewIdentifier(oldIdentifier, newIdentifier) {
+    const meta = await this.get(oldIdentifier, this._merkleTreeMetaStore)
+    if (!meta || !meta?.length) {
+      throw new Error(
+        `Merkle tree meta not found for identifier ${oldIdentifier}`
+      )
+    }
+
+    const treesMeta = meta.map(m => ({
+      ...m,
+      identifier: newIdentifier,
+    }))
+
+    await del(oldIdentifier, this._merkleTreeMetaStore)
+    await this.set(newIdentifier, treesMeta, this._merkleTreeMetaStore)
+    await this.set(oldIdentifier, newIdentifier, this._bindingStore)
+  }
+}
+
+class IndexedDBPrivateKeyStore {
+  static storageKey = "keystore"
+  constructor(aid, key) {
+    this.key = key
+    this.id = aid
+    this._store = createStore(
+      `${IndexedDBPrivateKeyStore.storageKey}-db`,
+      IndexedDBPrivateKeyStore.storageKey
+    )
+  }
+  async _get(key) {
+    return await decrypt(this.key, await get(key, this._store))
+  }
+  async _set(key, val) {
+    return await set(key, await encrypt(this.key, val), this._store)
+  }
+  async get(args) {
+    const key = await this._get(args.alias)
+    if (!key) {
+      throw new Error("no key under given alias")
+    }
+    return key.value
+  }
+  async importKey(args) {
+    await this._set(args.alias, { value: args.key })
   }
 }
