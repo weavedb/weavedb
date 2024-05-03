796431812f0034da3c937570e8a8229b10830547
Tomoya Nagasawa <ocrybit@gmail.com>
2024-05-03 17:24:30 +0400

    [sdk] option to apply api key

---
+++ sdk/sdk-web/warp-contracts-plugin-fetch-options.js
@@ -0,0 +1,27 @@
+const { WarpPlugin } = require("warp-contracts")
+
+module.exports = class FetchOptionsPlugin {
+  constructor(apiKey) {
+    this.apiKey = apiKey
+  }
+  process(request) {
+    const url = request.input
+
+    let fetchOptions = {}
+
+    if (
+      url == `https://d1o5nlqr4okus2.cloudfront.net/gateway/sequencer/register`
+    ) {
+      fetchOptions = { keepalive: true }
+    }
+    if (this.apiKey) {
+      fetchOptions.headers = request.init.headers
+      fetchOptions.headers["x-api-key"] = this.apiKey
+    }
+    return fetchOptions
+  }
+
+  type() {
+    return "fetch-options"
+  }
+}
