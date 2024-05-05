1676770a8f9175d45f4b619ad3e98d2e19da3b3b
Tomoya Nagasawa <ocrybit@gmail.com>
2024-05-05 19:22:08 +0400

    move grpc-node & explorer to weavedb/rdk

--- explorer/lib/nodes.js
+++
@@ -1,8 +0,0 @@
-export const nodes = [
-  {
-    endpoint: "rollup-testnet.weavedb.xyz:443",
-    network: "Private Alpha",
-    key: "alpha",
-  },
-  { endpoint: "localhost:8080", network: "Localhost", key: "localhost" },
-]
