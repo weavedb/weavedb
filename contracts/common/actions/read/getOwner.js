dc7ea7060b07ff1004202566d8a9f1f5ce1af3d9
Tomoya Nagasawa <ocrybit@gmail.com>
2024-05-05 19:17:39 +0400

    remove unnecessary files

--- contracts/common/actions/read/getOwner.js
+++
@@ -1,9 +0,0 @@
-const { is, of } = require("ramda")
-
-export const getOwner = async (state, action) => {
-  let owner = state.owner || []
-  if (is(String)(owner)) owner = of(owner)
-  return {
-    result: owner,
-  }
-}
