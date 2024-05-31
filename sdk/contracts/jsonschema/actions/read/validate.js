c608d554ad469636f7c392c1d443052bbdb5ca8d
Tomoya Nagasawa <ocrybit@gmail.com>
2024-05-31 10:50:38 +0400

    [contracts] fix jsonschema

--- sdk/contracts/jsonschema/actions/read/validate.js
+++ sdk/contracts/jsonschema/actions/read/validate.js
@@ -1,10 +1,10 @@
-const { valiate } = require("../../lib/jsonschema")
+const { validate: _validate } = require("../../lib/jsonschema")
 
 const validate = async (state, action) => {
   let valid = false
   let error = false
   try {
-    valid = validate(action.input.data, action.input.schema).valid
+    valid = _validate(action.input.data, action.input.schema).valid
   } catch (e) {
     error = true
   }
