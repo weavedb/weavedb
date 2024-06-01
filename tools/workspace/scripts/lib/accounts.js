52f03f451c7585b1dfec8b14c5cef6ede16708a3
Tomoya Nagasawa <ocrybit@gmail.com>
2024-06-01 09:26:39 +0400

    [tools] .gitignore weavedb.config.js

--- tools/workspace/scripts/lib/accounts.js
+++ tools/workspace/scripts/lib/accounts.js
@@ -6,7 +6,10 @@ const dir = resolve(__dirname, "../../.weavedb/accounts")
 const dir_evm = resolve(__dirname, "../../.weavedb/accounts/evm")
 const dir_ar = resolve(__dirname, "../../.weavedb/accounts/ar")
 let config = require("../../weavedb.config.js")
+config.accounts ??= {}
 const accounts = config.accounts
+config.accounts.evm ??= {}
+config.accounts.ar ??= {}
 for (const v of readdirSync(dir_evm)) {
   const acc = JSON.parse(readFileSync(resolve(dir_evm, v), "utf8"))
   const name = v.split(".")[0]
