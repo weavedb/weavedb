fbb03a4aa56d322f5261802423a837da6b31593e
Tomoya Nagasawa <ocrybit@gmail.com>
2025-05-10 02:08:24 +0400

    [hb] recover from HyperBEAM

---
+++ hb/src/recover.js
@@ -0,0 +1,42 @@
+import wdb from "./index.js"
+import { getKV, getMsgs } from "./server-utils.js"
+import { isEmpty } from "ramda"
+const recover = async ({ pid, jwk, dbpath, hb }) => {
+  let i = 0
+  let db = null
+  let from = 0
+  let to = 99
+  let res = await getMsgs({ pid, hb })
+  while (!isEmpty(res.assignments)) {
+    for (let k in res.assignments ?? {}) {
+      const m = res.assignments[k]
+      if (m.slot === 0) {
+        let from = null
+        for (const k in m.body.commitments) {
+          const c = m.body.commitments[k]
+          if (c.committer) {
+            from = c.committer
+            break
+          }
+        }
+        const wkv = getKV({ jwk, hb, dbpath, pid })
+        console.log("initializing...", pid)
+        db = wdb(wkv).init({ from, id: pid })
+      }
+      if (m.body.data) {
+        for (const v of JSON.parse(m.body.data)) {
+          const q = JSON.parse(v.query)
+          console.log("recovering...", q)
+          db.set(...q)
+          i++
+        }
+      }
+    }
+    from += 100
+    to += 100
+    res = await getMsgs({ pid, hb, from, to })
+  }
+  return db
+}
+
+export default recover
