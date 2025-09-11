f86b9465aee0f39540a2c835e425e2cc68cc068f
Tomoya Nagasawa <ocrybit@gmail.com>
2025-09-12 01:57:10 +0400

    [scan] update design

--- scan/components/Main.js
+++ scan/components/Main.js
@@ -3,18 +3,18 @@ import Footer from "./Footer"
 
 export default function Main({ children }) {
   return (
-    <Flex w="100%" pt="60px">
-      <Flex w="100%">
-        <Flex w="100%" bg="#eee" direction="column" minH="calc(100vh - 60px)">
-          <Flex w="100%" justify="center">
-            <Box w="100%" maxW="1360px">
-              {children}
-            </Box>
-          </Flex>
-          <Box flex={1} />
-          <Footer />
+    <Box w="100%" pt="90px" bg="#fafbfc" minH="100vh">
+      <Flex direction="column" minH="calc(100vh - 90px)">
+        {/* Main content area */}
+        <Flex justify="center" flex="1">
+          <Box w="100%" maxW="1200px" px={4}>
+            {children}
+          </Box>
         </Flex>
+
+        {/* Footer at bottom */}
+        <Footer />
       </Flex>
-    </Flex>
+    </Box>
   )
 }
