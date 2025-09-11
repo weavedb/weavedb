15465a79a7228a1ddcfbfa331eb1516cc6d3c462
Tomoya Nagasawa <ocrybit@gmail.com>
2025-09-11 13:27:37 +0400

    [docs] update wdb-core

--- core/src/index.js
+++ core/src/index.js
@@ -5,11 +5,11 @@ import vec from "./vec.js"
 import kv from "./kv.js"
 import kv_vec from "./kv_vec.js"
 import io from "./io.js"
+import queue from "./queue.js"
 import mem from "./mem.js"
 
 import bpt from "./bpt.js"
 import parser from "./parser.js"
 import build from "./build.js"
-import queue from "./queue.js"
 
 export { bpt, parser, build, queue, db, sql, vec, kv, kv_vec, io, mem }
