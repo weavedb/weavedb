81fa572fd7af6eccf2781df2fce8381f01073288
Tomoya Nagasawa <ocrybit@gmail.com>
2025-09-30 00:34:13 +0400

    [su] duplicate management

--- hb/src/su.js
+++ hb/src/su.js
@@ -6,6 +6,8 @@ import { ArweaveSigner } from "@ar.io/sdk"
 import { HB } from "wao"
 let procs = []
 let msgs = {}
+let ongoing = {}
+import { open } from "lmdb"
 export default class SU {
   constructor({
     port = 4003,
@@ -14,7 +16,9 @@ export default class SU {
     hb = "http://localhost:10001",
     mu = "https://mu.ao-testnet.xyz",
     bundler = "https://up.arweave.net:443/tx",
+    dbpath,
   }) {
+    this.io = open({ path: dbpath })
     this.hb = new HB({ format: "ans104", jwk })
     this.jwk = jwk
     this.bundler = bundler
@@ -42,69 +46,128 @@ export default class SU {
     app.post("/", async (req, res) => {
       const di = new DataItem(req.body)
       const _tags = tags(di.tags)
-      console.log(di.id, _tags)
-      if (msgs[di.id]) {
-        console.log("duplicate:", di.id)
+      if (!_tags["Query"] || !_tags["From-Process"] || !_tags["Reference"]) {
+        console.log("wrong query:", di.id)
+        res.status(400)
+        res.send({ id: di.id })
         return
       }
-      msgs[di.id] = di
-      res.status(201)
-      let slot = null
+      const id = `${_tags["From-Process"]}:${_tags["Reference"]}`
+      if (msgs[id]?.done || ongoing[id]) {
+        res.status(201)
+        res.send({ id: di.id })
+        console.log("duplicate:", id)
+        return
+      }
+      const exists = this.io.get(id)
+      if (exists?.done || ongoing[id]) {
+        msgs[id] = exists
+        res.status(201)
+        res.send({ id: di.id })
+        console.log("duplicate:", id)
+        return
+      }
+      console.log()
+      console.log("new: ", id)
+      console.log(_tags)
+      ongoing[id] = true
+      msgs[id] ??= { item: di }
       let _data = null
+      let slot = null
+      if (msgs[id].reply !== true) {
+        try {
+          const res2 = await this.hb.send104({
+            path: `/${di.target}/schedule`,
+            item: di,
+          })
+          if (res2?.status === 200) {
+            slot = res2.out.slot
+            const {
+              results: { data },
+            } = await this.hb.compute({ pid: di.target, slot })
+            if (typeof data !== "undefined") _data = data
+          }
+          msgs[id].reply = true
+          msgs[id].data = _data
+          msgs[id].slot = slot
+          await this.io.put(id, msgs[id])
+          res.status(201)
+          res.send({ id: di.id })
+        } catch (e) {
+          console.log(e)
+          res.status(500)
+          res.send({ id: di.id })
+        }
+      } else {
+        res.status(201)
+        res.send({ id: di.id })
+        _data = msgs[id].data
+        slot = msgs[id].slot
+      }
       try {
-        const res = await this.hb.send104({
-          path: `/${di.target}/schedule`,
-          item: di,
-        })
-        if (res?.status === 200) {
-          slot = res.out.slot
-          const {
-            results: { data },
-          } = await this.hb.compute({ pid: di.target, slot })
-          if (typeof data !== "undefined") _data = data
+        if (this.bundler && msgs[id].ar !== true) await this.toArweave(di, id)
+      } catch (e) {
+        console.log(e)
+      }
+      try {
+        if (msgs[id].aos !== true) {
+          const di2 = createData(JSON.stringify(_data), signer, {
+            target: _tags["From-Process"],
+            tags: [
+              { name: "Data-Protocol", value: "ao" },
+              { name: "Variant", value: "ao.TN.1" },
+              {
+                name: "From-Process",
+                value: di.target,
+              },
+              { name: "Type", value: "Message" },
+              { name: "X-Reference", value: _tags["Reference"] },
+              { name: "Reference", value: Number(slot).toString() },
+              { name: "Query", value: _tags.Query },
+            ],
+          })
+          await this.toAOS(di2, id)
         }
       } catch (e) {
         console.log(e)
       }
-      res.send({ slot, timestamp: 0 })
-      if (this.bundler) await this.toArweave(di)
-      const di2 = createData(JSON.stringify(_data), signer, {
-        target: _tags["From-Process"],
-        tags: [
-          { name: "Data-Protocol", value: "ao" },
-          { name: "Variant", value: "ao.TN.1" },
-          {
-            name: "From-Process",
-            value: di.target,
-          },
-          { name: "Type", value: "Message" },
-          { name: "X-Reference", value: _tags["Reference"] },
-          { name: "Reference", value: Number(slot).toString() },
-          { name: "Query", value: _tags.Query },
-        ],
-      })
-      await this.toAOS(di2)
+      if (
+        msgs[id].done !== true &&
+        msgs[id].aos &&
+        msgs[id].reply &&
+        msgs[id].ar
+      ) {
+        msgs[id].done = true
+        await this.io.put(id, msgs[id])
+      }
+      delete ongoing[id]
     })
     app.listen(port, () => console.log(`Server is running on port ${port}`))
   }
-  async toArweave(di) {
+  async toArweave(di, id) {
     const res = await fetch(this.bundler, {
       method: "POST",
       headers: { "Content-Type": "application/octet-stream" },
       body: di.binary,
     })
-    console.log(res)
+    const _tags = tags(di.tags)
+    console.log(`Arweave[${id}]: ${res.status}: ${_tags.Query}`)
+    if (res.status === 200) msgs[id].ar = true
+    else msgs[id].ar_error = res.status ?? 1
+    await this.io.put(id, msgs[id])
   }
-  async toAOS(di) {
+  async toAOS(di, id) {
     await di.sign(this.signer)
     const _tags = tags(di.tags)
-    console.log("toAOS", _tags)
     const res = await fetch(this.mu, {
       method: "POST",
       headers: { "Content-Type": "application/octet-stream" },
       body: di.binary,
     })
-    console.log(res)
+    console.log(`AOS[${id}]: ${res.status}: ${_tags.Query}`)
+    if (res.status === 202) msgs[id].aos = true
+    else msgs[id].aos_error = res?.status ?? 1
+    await this.io.put(id, msgs[id])
   }
   stop() {
     console.log(`SU is shutting down...`)
--- scripts/run-su.js
+++ scripts/run-su.js
@@ -12,12 +12,14 @@ let {
   mu = "https://mu.ao-testnet.xyz",
   wallet = "HyperBEAM/.wallet.json",
   bundler = null,
+  dbpath = ".db",
 } = yargs(process.argv.slice(2)).argv
 
 const main = async () => {
   const jwk = JSON.parse(readFileSync(resolve(process.cwd(), wallet), "utf8"))
+  const _dbpath = resolve(process.cwd(), dbpath, "su")
   console.log("signer:", toAddr(jwk.n))
-  const su = new SU({ port, jwk, db, hb, mu, bundler })
+  const su = new SU({ port, jwk, db, hb, mu, bundler, dbpath: _dbpath })
 }
 
 main()
