71c50d83de64595b5eff81cb4392d8d3e0cfe202
Tomoya Nagasawa <ocrybit@gmail.com>
2026-05-09 05:19:03 +0900

    feat(sdk): re-export wdb23 as `to23`
    hb/test/jots.test.js imports `to23` which was the old name for
    wdb23. Adding the alias unblocks tests that haven't been updated to
    the new name without breaking anything else.

--- sdk/src/index.js
+++ sdk/src/index.js
@@ -1,3 +1,6 @@
 import DB from "./db.js"
 export { wdb23, wdb160 } from "./utils.js"
+// `to23` was the previous name for `wdb23`; alias kept for tests that
+// haven't been updated to the new name.
+export { wdb23 as to23 } from "./utils.js"
 export { DB }
